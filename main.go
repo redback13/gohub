fe2ec6c1f05c88f077b44bcd47d2e125ca60032d
redback13 <372624741@qq.com>
2022-03-07 17:13:09 +0800

    初始化

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{"message": "OK"})
+	})
+
+	router.Run()
+}
